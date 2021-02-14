082389422b180ea62692a6b2a55cdbd27bacb1ad
mkantsurov <mkantsurov@gmail.com>
2021-02-15 00:49:45 +0200

    added new type of chart

--- src/main/java/technology/positivehome/ihome/server/persistence/MeasurementsLogRepositoryImpl.java
+++ src/main/java/technology/positivehome/ihome/server/persistence/MeasurementsLogRepositoryImpl.java
@@ -26,7 +26,7 @@ public class MeasurementsLogRepositoryImpl implements MeasurementsLogRepository 
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     private static final String SELECT_MEASUREMENT_LOG_ENTRY_FOR_PERIOD =
-            "SELECT id, created, load_avg, memory_heap_max, memory_heap_used, pressure, outdoor_temp, outdoor_humidity, indoor_sf_temp, indoor_sf_humidity, indoor_gf_temp, garage_temp, garage_humidity, boiler_temp, luminosity, power_stat " +
+            "SELECT id, created, load_avg, memory_heap_max, memory_heap_used, pressure, outdoor_temp, outdoor_humidity, indoor_sf_temp, indoor_sf_humidity, indoor_gf_temp, garage_temp, garage_humidity, boiler_temp, luminosity, power_stat, security_mode, pw_src_converter_mode, pw_src_direct_mode, heating_pump_ff_mode, heating_pump_sf_mode " +
                     "FROM measurements_log_entry " +
                     "WHERE created >= :start_time AND created < :end_time";
 
