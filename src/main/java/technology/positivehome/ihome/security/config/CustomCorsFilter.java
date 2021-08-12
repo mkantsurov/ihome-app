f84b0712710b8ac4d05e54e1a9fc49cc3d332cd6
mkantsurov <mkantsurov@gmail.com>
2021-08-12 09:16:20 +0300

    added new module

--- src/main/java/technology/positivehome/ihome/security/config/CustomCorsFilter.java
+++ src/main/java/technology/positivehome/ihome/security/config/CustomCorsFilter.java
@@ -5,6 +5,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class CustomCorsFilter extends CorsFilter {
 
@@ -15,7 +16,7 @@ public class CustomCorsFilter extends CorsFilter {
     private static UrlBasedCorsConfigurationSource configurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.setAllowedOriginPatterns(List.of("http://localhost:4200", "www.positivehome.technology", "positivehome.technology"));
         config.addAllowedHeader("*");
         config.setMaxAge(36000L);
         config.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"));
