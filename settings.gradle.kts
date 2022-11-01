c1933a75d094e9efc573cdcc8904f7fb973dda52
mkantsurov <mkantsurov@gmail.com>
2022-11-01 15:17:09 +0200

    updated spring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 pluginManagement {
-    val springBootVersion: String = "2.5.12"
+    val springBootVersion: String = "2.7.5"
     plugins {
         id("org.springframework.boot") version springBootVersion
     }
