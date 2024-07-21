bc258fec97cee96c0cab665836da961e4e030527
mkantsurov <mkantsurov@gmail.com>
2024-07-22 00:50:46 +0300

    boot upgrade

--- src/main/java/technology/positivehome/ihome/server/service/core/module/RecuperationPowerControlModule.java
+++ src/main/java/technology/positivehome/ihome/server/service/core/module/RecuperationPowerControlModule.java
@@ -10,28 +10,21 @@ import technology.positivehome.ihome.server.model.command.IHomeCommandFactory;
 import technology.positivehome.ihome.server.service.core.SystemManager;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class RecuperationPowerControlModule extends AbstractRelayBasedIHomeModule implements IHomeModule {
 
     private static final Log log = LogFactory.getLog(RecuperationPowerControlModule.class);
-    private static final long POWER_CHECK_INTERVAL = TimeUnit.SECONDS.toMillis(120);
-    private static final long MAX_POWER_ABSENT_DELAY = TimeUnit.MINUTES.toMillis(3);
-    private static final long POWER_CHECKING_DELAY = TimeUnit.MINUTES.toMillis(3);
-
+    private static final long TEMPERATURE_CHECK_INTERVAL = TimeUnit.SECONDS.toMillis(120);
     private static final long OUTDOOR_TEMPERATURE_SENS_PORT_ID = 11;
 
     public static final int POWER_SENSOR_PORT_ID = 29;
 
     private final CronModuleJob[] moduleJobs;
 
-    private final AtomicLong lastPowerOkTs = new AtomicLong(System.currentTimeMillis());
-    private final AtomicLong lastPowerFailTs = new AtomicLong(System.currentTimeMillis());
-
     public RecuperationPowerControlModule(SystemManager mgr, ModuleConfigEntry configEntry) {
         super(mgr, configEntry);
         moduleJobs = new CronModuleJob[]{
-                new CronModuleJob(POWER_CHECK_INTERVAL) {
+                new CronModuleJob(TEMPERATURE_CHECK_INTERVAL) {
                     @Override
                     protected void execute() throws Exception {
                         switch (getMode()) {
@@ -43,22 +36,18 @@ public class RecuperationPowerControlModule extends AbstractRelayBasedIHomeModul
                                 log.info("RecuperationModule: " +
                                         "\n ext power state: " + state +
                                         "\n module out port state: " + status.isEnabled() +
-                                        "\n last power ok ts: " + (System.currentTimeMillis() - lastPowerOkTs.get()) +
                                         "\n temperature: " + data.getTemperature());
                                 switch (state) {
                                     case ENABLED:
-                                        lastPowerOkTs.set(System.currentTimeMillis());
                                         if (status.isDisabled()
-                                                && now - POWER_CHECKING_DELAY > lastPowerFailTs.get()
-                                                && data.getTemperature() < 24.5) {
+                                            && data.getTemperature() < 24.5) {
                                             setOutputStatus(OutputPortStatus.enabled());
                                         }  else if (status.isEnabled() && data.getTemperature() > 25) {
                                             setOutputStatus(OutputPortStatus.disabled());
                                         }
                                         break;
                                     case DISABLED:
-                                        lastPowerFailTs.set(System.currentTimeMillis());
-                                        if (status.isEnabled() && now - MAX_POWER_ABSENT_DELAY > lastPowerOkTs.get()) {
+                                        if (status.isEnabled()) {
                                             setOutputStatus(OutputPortStatus.disabled());
                                         }
                                         break;
